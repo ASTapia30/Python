7c3b8fa20f825fd08e50a50b6486c7087ff3cd4f
Alfonso Serrano <alserrano92@gmail.com>
2022-01-31 17:59:52 -0500

    .

---
+++ Ejercicios/Ejercicio4.py
@@ -0,0 +1,3 @@
+'''Escriba un programa que busca la letra mas común dentro de una cadena recibida, cuál es la
+letra que aparece más veces en la cadena. La respuesta debe ser una letra, no puede ser un
+carácter de puntuación, o un espacio en blanco.'''
---
+++ Ejercicios/Ejercicio5.py
@@ -0,0 +1,10 @@
+'''Existe un lenguaje construido a partir de la transformación de palabras en inglés. Aunque se
+desconoce el origen de dicho lenguaje, este es mencionado en al menos dos documentos delsiglo XIX, lo cual sugiere que ha existido por más de cien años. Las siguientes son las reglas
+que se usan para traducir del inglés a dicho lenguaje:
+• Si una palabra empieza por una consonante, todas las letras del inicio de la palabra, hasta
+la primera vocal, se eliminan y luego se agregan al final de la palabra, seguidas de “ay”.
+Por ejemplo, “computer” se traduce a “omputercay” y “think” se traduce a “inkthay”.
+• Si una palabra inicia por vocal, se agrega “way” al final de la palabra. Por ejemplo,
+“algorithm” se traduce a “algorithmway” y “office” se convierte en “officeway”.
+Escriba un programa que reciba un texto, y muestre la traducción de dicho texto a dicho
+lenguaje.'''
